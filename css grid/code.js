e0610df2563c3e72b07c3def7c109c528c5f91b2
AlexeyVolnov <57610468+AlexeyVolnov@users.noreply.github.com>
2020-11-03 19:30:42 +0500

    свертстал страницу на гридах для реакта в папке grid css

---
+++ css grid/code.js
@@ -0,0 +1,9 @@
+// let wrapperAll = document.querySelector(".wrapper-all");
+// let menu = document.querySelectorAll(".menu");
+//
+// console.log(menu)
+//
+// menu[0].addEventListener("click",function () {
+//   console.log(wrapperAll.style.backgroundImage);
+//   wrapperAll.style.backgroundImage = "url(images/hfpv.jpg)"
+// })
