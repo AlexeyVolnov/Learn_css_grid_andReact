97bd15988e8be5b108eb21a74f7a453a1c0a8f77
AlexeyVolnov <57610468+AlexeyVolnov@users.noreply.github.com>
2020-11-04 12:23:08 +0500

    Перенес страницу из папки css grid в приложение папки react

--- Reactapp/app/src/App.js
+++ Reactapp/app/src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper-all grid" style="background-image: url(images/les_vid_sverhu_ten_191553_1920x1080.jpg)">
+
+        <header>
+          <div>Search</div>
+        </header>
+        <article>
+          <div className="content">
+            <div>Здесь контент <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolor eum itaque non
+              officia placeat quaerat. Eaque fugit magni ullam. Asperiores dolorum ducimus expedita quis totam! Delectus
+              quam recusandae voluptas.</div>
+            </div>
+          </div>
+        </article>
+        <div className="menu-all">
+          <div className="menu">Мой профиль</div>
+          <div className="menu">Новости</div>
+          <div className="menu">Друзья</div>
+          <div className="menu">Сообщения</div>
+          <div className="menu">Музыка</div>
+          <div className="menu">Настройки</div>
+        </div>
+      </div>
   );
 }
 
---
+++ react/app/src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper-all grid">
+
+        <header>
+          <div>Search</div>
+        </header>
+        <article>
+          <div className="content">
+            <div>Здесь контент <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolor eum itaque non
+              officia placeat quaerat. Eaque fugit magni ullam. Asperiores dolorum ducimus expedita quis totam! Delectus
+              quam recusandae voluptas.</div>
+              <div>Aperiam at beatae consequuntur deleniti esse expedita explicabo iste labore laborum libero nam, nemo
+                nesciunt officiis omnis pariatur, possimus praesentium quia quibusdam quidem repudiandae sunt temporibus
+                velit, veniam vero voluptates?
+              </div>
+              <div>A assumenda cum debitis deserunt dolor doloremque eius eum expedita facere harum impedit, in
+                necessitatibus nesciunt numquam odit officia optio placeat porro quae repellendus rerum sequi soluta
+                suscipit velit veniam.
+              </div>
+              <div>Aut blanditiis non voluptatum. A architecto asperiores assumenda consectetur dolore earum ex
+                exercitationem hic maiores mollitia natus officia quaerat quas quia rem, saepe sit, soluta sunt ut vero
+                vitae, voluptates.
+              </div>
+              <div>Accusamus accusantium asperiores blanditiis culpa cum cumque deleniti dolor dolorem eius est harum
+                laudantium libero magni, mollitia nobis nulla odit omnis quam quod reprehenderit tempore unde, ut veniam
+                veritatis voluptate?
+              </div>
+            </div>
+          </div>
+        </article>
+        <div className="menu-all">
+          <div className="menu">Мой профиль</div>
+          <div className="menu">Новости</div>
+          <div className="menu">Друзья</div>
+          <div className="menu">Сообщения</div>
+          <div className="menu">Музыка</div>
+          <div className="menu">Настройки</div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
